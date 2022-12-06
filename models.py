d7b3c9e04df54e40497e7355e066133ae83d72c1
Shreeram Narayanan <shreeramramu.06@gmail.com>
2022-12-06 02:47:57 -0800

    Decoder architecture added

--- models.py
+++ models.py
@@ -26,8 +26,15 @@ def build_or_load_gen_model(args):
     tokenizer = tokenizer_class.from_pretrained(args.tokenizer_name)
     if args.model_type == 'roberta':
         encoder = model_class.from_pretrained(args.model_name_or_path, config=config)
-        decoder_layer = nn.TransformerDecoderLayer(d_model=config.hidden_size, nhead=config.num_attention_heads)
-        decoder = nn.TransformerDecoder(decoder_layer, num_layers=6)
+        # encoder_layer = nn.TransformerEncoderLayer(d_model=config.hidden_size, nhead=config.num_attention_heads)
+        # encoder = nn.TransformerEncoder(encoder_layer, num_layers=6)
+        print(encoder)
+        print("hidden size", config.hidden_size)
+        # decoder_layer = nn.TransformerDecoderLayer(d_model=config.hidden_size, nhead=config.num_attention_heads)
+        # decoder = nn.TransformerDecoder(decoder_layer, num_layers=6)
+        decoder_layer = DecoderLayer(d_model=config.hidden_size, ffn_hidden=2048, n_head=config.num_attention_heads, drop_prob=0.1)
+        decoder = Decoder(config.vocab_size, 512, d_model=config.hidden_size, ffn_hidden=2048, n_head=config.num_attention_heads, n_layers=6, drop_prob=0.2, device='cuda')
+        # dec_voc_size, max_len, d_model, n_head, n_layers, drop_prob, device
         model = Seq2Seq(encoder=encoder, decoder=decoder, config=config,
                         beam_size=args.beam_size, max_length=args.max_target_length,
                         sos_id=tokenizer.cls_token_id, eos_id=tokenizer.sep_token_id)
@@ -57,128 +64,6 @@ class RobertaClassificationHead(nn.Module):
         x = torch.tanh(x)
         x = self.out_proj(x)
         return x
-
-
-class CloneModel(nn.Module):
-    def __init__(self, encoder, config, tokenizer, args):
-        super(CloneModel, self).__init__()
-        self.encoder = encoder
-        self.config = config
-        self.tokenizer = tokenizer
-        self.classifier = RobertaClassificationHead(config)
-        self.args = args
-
-    def get_t5_vec(self, source_ids):
-        attention_mask = source_ids.ne(self.tokenizer.pad_token_id)
-        outputs = self.encoder(input_ids=source_ids, attention_mask=attention_mask,
-                               labels=source_ids, decoder_attention_mask=attention_mask, output_hidden_states=True)
-        hidden_states = outputs['decoder_hidden_states'][-1]
-        eos_mask = source_ids.eq(self.config.eos_token_id)
-
-        if len(torch.unique(eos_mask.sum(1))) > 1:
-            raise ValueError("All examples must have the same number of <eos> tokens.")
-        vec = hidden_states[eos_mask, :].view(hidden_states.size(0), -1,
-                                              hidden_states.size(-1))[:, -1, :]
-        return vec
-
-    def get_bart_vec(self, source_ids):
-        attention_mask = source_ids.ne(self.tokenizer.pad_token_id)
-        outputs = self.encoder(input_ids=source_ids, attention_mask=attention_mask,
-                               labels=source_ids, decoder_attention_mask=attention_mask, output_hidden_states=True)
-        hidden_states = outputs['decoder_hidden_states'][-1]
-        eos_mask = source_ids.eq(self.config.eos_token_id)
-
-        if len(torch.unique(eos_mask.sum(1))) > 1:
-            raise ValueError("All examples must have the same number of <eos> tokens.")
-        vec = hidden_states[eos_mask, :].view(hidden_states.size(0), -1,
-                                              hidden_states.size(-1))[:, -1, :]
-        return vec
-
-    def get_roberta_vec(self, source_ids):
-        attention_mask = source_ids.ne(self.tokenizer.pad_token_id)
-        vec = self.encoder(input_ids=source_ids, attention_mask=attention_mask)[0][:, 0, :]
-        return vec
-
-    def forward(self, source_ids=None, labels=None):
-        source_ids = source_ids.view(-1, self.args.max_source_length)
-
-        if self.args.model_type == 'codet5':
-            vec = self.get_t5_vec(source_ids)
-        elif self.args.model_type == 'bart':
-            vec = self.get_bart_vec(source_ids)
-        elif self.args.model_type == 'roberta':
-            vec = self.get_roberta_vec(source_ids)
-
-        logits = self.classifier(vec)
-        prob = nn.functional.softmax(logits)
-
-        if labels is not None:
-            loss_fct = nn.CrossEntropyLoss()
-            loss = loss_fct(logits, labels)
-            return loss, prob
-        else:
-            return prob
-
-
-class DefectModel(nn.Module):
-    def __init__(self, encoder, config, tokenizer, args):
-        super(DefectModel, self).__init__()
-        self.encoder = encoder
-        self.config = config
-        self.tokenizer = tokenizer
-        self.classifier = nn.Linear(config.hidden_size, 2)
-        self.args = args
-
-    def get_t5_vec(self, source_ids):
-        attention_mask = source_ids.ne(self.tokenizer.pad_token_id)
-        outputs = self.encoder(input_ids=source_ids, attention_mask=attention_mask,
-                               labels=source_ids, decoder_attention_mask=attention_mask, output_hidden_states=True)
-        hidden_states = outputs['decoder_hidden_states'][-1]
-        eos_mask = source_ids.eq(self.config.eos_token_id)
-
-        if len(torch.unique(eos_mask.sum(1))) > 1:
-            raise ValueError("All examples must have the same number of <eos> tokens.")
-        vec = hidden_states[eos_mask, :].view(hidden_states.size(0), -1,
-                                              hidden_states.size(-1))[:, -1, :]
-        return vec
-
-    def get_bart_vec(self, source_ids):
-        attention_mask = source_ids.ne(self.tokenizer.pad_token_id)
-        outputs = self.encoder(input_ids=source_ids, attention_mask=attention_mask,
-                               labels=source_ids, decoder_attention_mask=attention_mask, output_hidden_states=True)
-        hidden_states = outputs['decoder_hidden_states'][-1]
-        eos_mask = source_ids.eq(self.config.eos_token_id)
-
-        if len(torch.unique(eos_mask.sum(1))) > 1:
-            raise ValueError("All examples must have the same number of <eos> tokens.")
-        vec = hidden_states[eos_mask, :].view(hidden_states.size(0), -1,
-                                              hidden_states.size(-1))[:, -1, :]
-        return vec
-
-    def get_roberta_vec(self, source_ids):
-        attention_mask = source_ids.ne(self.tokenizer.pad_token_id)
-        vec = self.encoder(input_ids=source_ids, attention_mask=attention_mask)[0][:, 0, :]
-        return vec
-
-    def forward(self, source_ids=None, labels=None):
-        source_ids = source_ids.view(-1, self.args.max_source_length)
-
-        if self.args.model_type == 'codet5':
-            vec = self.get_t5_vec(source_ids)
-        elif self.args.model_type == 'bart':
-            vec = self.get_bart_vec(source_ids)
-        elif self.args.model_type == 'roberta':
-            vec = self.get_roberta_vec(source_ids)
-
-        logits = self.classifier(vec)
-        prob = nn.functional.softmax(logits)
-
-        if labels is not None:
-            loss_fct = nn.CrossEntropyLoss()
-            loss = loss_fct(logits, labels)
-            return loss, prob
-        else:
-            return prob
 
 
 # https://github.com/microsoft/CodeBERT/blob/master/CodeBERT/code2nl/model.py
@@ -234,8 +119,19 @@ class Seq2Seq(nn.Module):
         if target_ids is not None:
             attn_mask = -1e4 * (1 - self.bias[:target_ids.shape[1], :target_ids.shape[1]])
             tgt_embeddings = self.encoder.embeddings(target_ids).permute([1, 0, 2]).contiguous()
-            out = self.decoder(tgt_embeddings, encoder_output, tgt_mask=attn_mask,
-                               memory_key_padding_mask=~source_mask)
+            tgt_embeddings = tgt_embeddings.type(torch.LongTensor)
+            # tgt_embeddings = tgt_embeddings.cuda()
+            print("tgt emb", tgt_embeddings.is_cuda)
+
+            print("tgt emb", tgt_embeddings.size())
+            print("enc op", encoder_output.size())
+            print("attn mask", attn_mask.size())
+            print("source msk", source_mask.size())
+
+            # out = self.decoder(tgt_embeddings, encoder_output, tgt_mask=attn_mask,
+            #                    memory_key_padding_mask=~source_mask)
+            
+            out = self.decoder(tgt_embeddings, encoder_output, attn_mask, source_mask)
             # memory_key_padding_mask=(1 - source_mask).bool())
             hidden_states = torch.tanh(self.dense(out)).permute([1, 0, 2]).contiguous()
             lm_logits = self.lm_head(hidden_states)
@@ -266,8 +162,11 @@ class Seq2Seq(nn.Module):
                         break
                     attn_mask = -1e4 * (1 - self.bias[:input_ids.shape[1], :input_ids.shape[1]])
                     tgt_embeddings = self.encoder.embeddings(input_ids).permute([1, 0, 2]).contiguous()
-                    out = self.decoder(tgt_embeddings, context, tgt_mask=attn_mask,
-                                       memory_key_padding_mask=~context_mask)
+                    # out = self.decoder(tgt_embeddings, context, tgt_mask=attn_mask,
+                    #                    memory_key_padding_mask=~context_mask)
+                    tgt_embeddings = tgt_embeddings.type(torch.LongTensor)
+                    tgt_embeddings = tgt_embeddings.cuda()
+                    out = self.decoder(tgt_embeddings, context, attn_mask, context_mask)
                     # memory_key_padding_mask=(1 - context_mask).bool())
                     out = torch.tanh(self.dense(out))
                     hidden_states = out.permute([1, 0, 2]).contiguous()[:, -1, :]
@@ -396,3 +295,294 @@ class Beam(object):
                 tokens.append(tok)
             sentence.append(tokens)
         return sentence
+
+
+class DecoderLayer(nn.Module):
+
+    def __init__(self, d_model, ffn_hidden, n_head, drop_prob):
+        super(DecoderLayer, self).__init__()
+        self.self_attention = MultiHeadAttention(d_model=d_model, n_head=n_head)
+        self.norm1 = LayerNorm(d_model=d_model)
+        self.dropout1 = nn.Dropout(p=drop_prob)
+
+        self.enc_dec_attention = MultiHeadAttention(d_model=d_model, n_head=n_head)
+        self.norm2 = LayerNorm(d_model=d_model)
+        self.dropout2 = nn.Dropout(p=drop_prob)
+
+        self.ffn = PositionwiseFeedForward(d_model=d_model, hidden=ffn_hidden, drop_prob=drop_prob)
+        self.norm3 = LayerNorm(d_model=d_model)
+        self.dropout3 = nn.Dropout(p=drop_prob)
+
+    def forward(self, dec, enc, trg_mask, src_mask):    
+        # 1. compute self attention
+        _x = dec
+        x = self.self_attention(q=dec, k=dec, v=dec, mask=trg_mask)
+        
+        # 2. add and norm
+        x = self.norm1(x + _x)
+        x = self.dropout1(x)
+
+        if enc is not None:
+            # 3. compute encoder - decoder attention
+            _x = x
+            x = self.enc_dec_attention(q=x, k=enc, v=enc, mask=src_mask)
+            
+            # 4. add and norm
+            x = self.norm2(x + _x)
+            x = self.dropout2(x)
+
+        # 5. positionwise feed forward network
+        _x = x
+        x = self.ffn(x)
+        
+        # 6. add and norm
+        x = self.norm3(x + _x)
+        x = self.dropout3(x)
+        return x
+
+
+class Decoder(nn.Module):
+    def __init__(self, dec_voc_size, max_len, d_model, ffn_hidden, n_head, n_layers, drop_prob, device):
+        super().__init__()
+        self.emb = TransformerEmbedding(d_model=d_model,
+                                        drop_prob=drop_prob,
+                                        max_len=max_len,
+                                        vocab_size=dec_voc_size,
+                                        device=device)
+
+        self.layers = nn.ModuleList([DecoderLayer(d_model=d_model,
+                                                  ffn_hidden=ffn_hidden,
+                                                  n_head=n_head,
+                                                  drop_prob=drop_prob)
+                                     for _ in range(n_layers)])
+
+        self.linear = nn.Linear(d_model, dec_voc_size)
+
+    def forward(self, trg, src, trg_mask, src_mask):
+        print(trg)
+        print(type(trg))
+        print(trg.size())
+        trg = self.emb(trg)
+        print("Hello")
+        # trg = int(trg)
+        for layer in self.layers:
+            trg = layer(trg, src, trg_mask, src_mask)
+
+        # pass to LM head
+        output = self.linear(trg)
+        return output
+
+
+class PositionwiseFeedForward(nn.Module):
+
+    def __init__(self, d_model, hidden, drop_prob=0.1):
+        super(PositionwiseFeedForward, self).__init__()
+        self.linear1 = nn.Linear(d_model, hidden)
+        self.linear2 = nn.Linear(hidden, d_model)
+        self.relu = nn.ReLU()
+        self.dropout = nn.Dropout(p=drop_prob)
+
+    def forward(self, x):
+        x = self.linear1(x)
+        x = self.relu(x)
+        x = self.dropout(x)
+        x = self.linear2(x)
+        return x
+
+
+class MultiHeadAttention(nn.Module):
+
+    def __init__(self, d_model, n_head):
+        super(MultiHeadAttention, self).__init__()
+        self.n_head = n_head
+        self.attention = ScaleDotProductAttention()
+        self.w_q = nn.Linear(d_model, d_model)
+        self.w_k = nn.Linear(d_model, d_model)
+        self.w_v = nn.Linear(d_model, d_model)
+        self.w_concat = nn.Linear(d_model, d_model)
+
+    def forward(self, q, k, v, mask=None):
+        # 1. dot product with weight matrices
+        q, k, v = self.w_q(q), self.w_k(k), self.w_v(v)
+
+        # 2. split tensor by number of heads
+        q, k, v = self.split(q), self.split(k), self.split(v)
+
+        # 3. do scale dot product to compute similarity
+        out, attention = self.attention(q, k, v, mask=mask)
+        
+        # 4. concat and pass to linear layer
+        out = self.concat(out)
+        out = self.w_concat(out)
+
+        # 5. visualize attention map
+        # TODO : we should implement visualization
+
+        return out
+
+    def split(self, tensor):
+        """
+        split tensor by number of head
+
+        :param tensor: [batch_size, length, d_model]
+        :return: [batch_size, head, length, d_tensor]
+        """
+        batch_size, length, d_model = tensor.size()
+
+        d_tensor = d_model // self.n_head
+        tensor = tensor.view(batch_size, length, self.n_head, d_tensor).transpose(1, 2)
+        # it is similar with group convolution (split by number of heads)
+
+        return tensor
+
+    def concat(self, tensor):
+        """
+        inverse function of self.split(tensor : torch.Tensor)
+
+        :param tensor: [batch_size, head, length, d_tensor]
+        :return: [batch_size, length, d_model]
+        """
+        batch_size, head, length, d_tensor = tensor.size()
+        d_model = head * d_tensor
+
+        tensor = tensor.transpose(1, 2).contiguous().view(batch_size, length, d_model)
+        return tensor
+
+
+class TransformerEmbedding(nn.Module):
+    """
+    token embedding + positional encoding (sinusoid)
+    positional encoding can give positional information to network
+    """
+
+    def __init__(self, vocab_size, d_model, max_len, drop_prob, device):
+        """
+        class for word embedding that included positional information
+        :param vocab_size: size of vocabulary
+        :param d_model: dimensions of model
+        """
+        super(TransformerEmbedding, self).__init__()
+        self.tok_emb = TokenEmbedding(vocab_size, d_model)
+        self.pos_emb = PostionalEncoding(d_model, max_len, device)
+        self.drop_out = nn.Dropout(p=drop_prob)
+
+    def forward(self, x):
+        print("x", x)
+        print("type x", type(x))
+        print("x shape", x.size()) 
+        print("x cuda", x.is_cuda)
+        x = x.cuda()
+        tok_emb = self.tok_emb(x)
+        print(tok_emb)
+        print("type of tok emb", type(tok_emb))
+        pos_emb = self.pos_emb(x)
+        return self.drop_out(tok_emb + pos_emb)
+
+
+class PostionalEncoding(nn.Module):
+    """
+    compute sinusoid encoding.
+    """
+
+    def __init__(self, d_model, max_len, device):
+        """
+        constructor of sinusoid encoding class
+        :param d_model: dimension of model
+        :param max_len: max sequence length
+        :param device: hardware device setting
+        """
+        super(PostionalEncoding, self).__init__()
+
+        # same size with input matrix (for adding with input matrix)
+        self.encoding = torch.zeros(max_len, d_model, device=device)
+        self.encoding.requires_grad = False  # we don't need to compute gradient
+
+        pos = torch.arange(0, max_len, device=device)
+        pos = pos.float().unsqueeze(dim=1)
+        # 1D => 2D unsqueeze to represent word's position
+
+        _2i = torch.arange(0, d_model, step=2, device=device).float()
+        # 'i' means index of d_model (e.g. embedding size = 50, 'i' = [0,50])
+        # "step=2" means 'i' multiplied with two (same with 2 * i)
+
+        self.encoding[:, 0::2] = torch.sin(pos / (10000 ** (_2i / d_model)))
+        self.encoding[:, 1::2] = torch.cos(pos / (10000 ** (_2i / d_model)))
+        # compute positional encoding to consider positional information of words
+
+    def forward(self, x):
+        # self.encoding
+        # [max_len = 512, d_model = 512]
+
+        batch_size, seq_len = x.size()
+        # [batch_size = 128, seq_len = 30]
+
+        return self.encoding[:seq_len, :]
+        # [seq_len = 30, d_model = 512]
+        # it will add with tok_emb : [128, 30, 512]
+
+
+class TokenEmbedding(nn.Embedding):
+    """
+    Token Embedding using torch.nn
+    they will dense representation of word using weighted matrix
+    """
+
+    def __init__(self, vocab_size, d_model):
+        """
+        class for token embedding that included positional information
+        :param vocab_size: size of vocabulary
+        :param d_model: dimensions of model
+        """
+        super(TokenEmbedding, self).__init__(vocab_size, d_model, padding_idx=1)
+
+
+class ScaleDotProductAttention(nn.Module):
+    """
+    compute scale dot product attention
+
+    Query : given sentence that we focused on (decoder)
+    Key : every sentence to check relationship with Qeury(encoder)
+    Value : every sentence same with Key (encoder)
+    """
+
+    def __init__(self):
+        super(ScaleDotProductAttention, self).__init__()
+        self.softmax = nn.Softmax(dim=-1)
+
+    def forward(self, q, k, v, mask=None, e=1e-12):
+        # input is 4 dimension tensor
+        # [batch_size, head, length, d_tensor]
+        batch_size, head, length, d_tensor = k.size()
+
+        # 1. dot product Query with Key^T to compute similarity
+        k_t = k.transpose(2, 3)  # transpose
+        score = (q @ k_t) / math.sqrt(d_tensor)  # scaled dot product
+
+        # 2. apply masking (opt)
+        if mask is not None:
+            score = score.masked_fill(mask == 0, -e)
+
+        # 3. pass them softmax to make [0, 1] range
+        score = self.softmax(score)
+
+        # 4. multiply with Value
+        v = score @ v
+
+        return v, score
+
+
+class LayerNorm(nn.Module):
+    def __init__(self, d_model, eps=1e-12):
+        super(LayerNorm, self).__init__()
+        self.gamma = nn.Parameter(torch.ones(d_model))
+        self.beta = nn.Parameter(torch.zeros(d_model))
+        self.eps = eps
+
+    def forward(self, x):
+        mean = x.mean(-1, keepdim=True)
+        std = x.std(-1, keepdim=True)
+        # '-1' means last dimension. 
+
+        out = (x - mean) / (std + self.eps)
+        out = self.gamma * out + self.beta
+        return out
